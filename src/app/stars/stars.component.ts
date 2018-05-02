38e7824ef21db25add33448e2969dc256148b711
Dan Dohotaru <dan.dohotaru@gmail.com>
2018-05-02 20:54:16 +0200

    implement ControlValueAccessor for stars

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { forwardRef, HostBinding } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const StarProvider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => StarsComponent),
+  multi: true
+}
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
-  styleUrls: ['./stars.component.css']
+  styleUrls: ['./stars.component.css'],
+  providers: [StarProvider],
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements ControlValueAccessor, OnInit {
+
+  @Input()
+  public disabled = false;
+
+  @HostBinding('style.opacity')
+  get opacity() {
+    return this.disabled ? 0.25 : 1;
+  }
 
   public stars: boolean[] = Array(5).fill(false);
+
+  public onChange = (rating: number) => { };
+
+  public onTouched = () => { };
 
   public ngOnInit(): void {
   }
@@ -19,10 +40,35 @@ export class StarsComponent implements OnInit {
   }
 
   public rate(rating: number) {
-    this.stars = this.stars.map((_, i) => rating > i);
+    if (!this.disabled) {
+      this.writeValue(rating);
+    }
   }
 
-  public click(starred, i) {
-    this.rate(i + (starred ? (this.value > i + 1 ? 1 : 0) : 1))
+  public writeValue(rating: number): void {
+    this.stars = this.stars.map((star, index) => rating > index);
+    this.onChange(this.value)
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public registerOnChange(fn: (rating: number) => void): void {
+    this.onChange = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  public click(star: boolean, index: number) {
+    this.onTouched();
+    let current = star
+      ? this.value > index + 1
+        ? 1
+        : 0
+      : 1;
+    this.rate(index + current);
   }
 }
