28e44e54911094a50c93f8db8b996b0dbc39d3bf
Dan Dohotaru <dan.dohotaru@gmail.com>
2018-05-04 12:41:36 +0200

    implement ngmodel support for markdown editor based on simplemde

--- src/app/markdown/markdown.component.ts
+++ src/app/markdown/markdown.component.ts
@@ -1,40 +1,92 @@
-import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild, ElementRef, InjectionToken, Inject } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { ViewChild, ElementRef, InjectionToken, Inject, } from '@angular/core';
 import * as SimpleMDE from 'simplemde';
 
 @Component({
   selector: 'app-markdown',
   templateUrl: './markdown.component.html',
-  styleUrls:['./markdown.component.css']
+  styleUrls: ['./markdown.component.css']
 })
 export class MarkdownComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  @ViewChild('simplemde') 
-  public textarea: ElementRef;
+  @ViewChild('placeholder')
+  public placeholder: ElementRef;
 
-  private simplemde: SimpleMDE;
+  @Input()
+  public model: any;
+
+  @Output()
+  public modelChange = new EventEmitter<string>();
+
+  private editor: SimpleMDE;
 
   constructor() {
-    
-   }
+
+  }
 
   public ngOnInit() {
   }
 
   public ngAfterViewInit(): void {
     let config = {
-      element : this.textarea.nativeElement,
+      element: this.placeholder.nativeElement,
       autoDownloadFontAwesome: false,
-      spellChecker:false,
+      spellChecker: false,
+      forceSync: true,
+      status: false,
+      autofocus: true,
+      tabSize: 4,
     };
-    this.simplemde = new SimpleMDE(config);
+    this.editor = new SimpleMDE(config);
+    this.editor.value(this.model || "")
 
-  //   this.ngZone.runOutsideAngular(() => {
-  //     // code which configures and creates SimpleMDE
-  // });
+    this.editor.codemirror.on('change', (context, change) => {
+      const value = context.getValue();
+      this.modelChange.emit(value);
+    });
   }
 
   public ngOnDestroy(): void {
-    this.simplemde = null
+    this.editor = null
   }
 
+  private mark(): string {
+    var content = this.editor.value();
+    return content;
+  }
+
+  private html(): string {
+    var content = this.editor.value();
+    return this.editor.markdown(content);
+  }
+
+  private text(): string {
+    var content = this.editor.value();
+    // based on npm module remove-markdown 0.0.6
+    var output = content
+      // strip list leaders
+      .replace(/^([\s\t]*)([\*\-\+]|\d\.)\s+/gm, '$1')
+      // Remove HTML tags
+      .replace(/<(.*?)>/g, '$1')
+      // Remove setext-style headers
+      .replace(/^[=\-]{2,}\s*$/g, '')
+      // Remove footnotes?
+      .replace(/\[\^.+?\](\: .*?$)?/g, '')
+      .replace(/\s{0,2}\[.*?\]: .*?$/g, '')
+      // Remove images
+      .replace(/\!\[.*?\][\[\(].*?[\]\)]/g, '')
+      // Remove inline links
+      .replace(/\[(.*?)\][\[\(].*?[\]\)]/g, '$1')
+      // Remove reference-style links?
+      .replace(/^\s{1,2}\[(.*?)\]: (\S+)( ".*?")?\s*$/g, '')
+      // Remove atx-style headers
+      .replace(/^\#{1,6}\s*([^#]*)\s*(\#{1,6})?/gm, '$1')
+      .replace(/([\*_]{1,2})(\S.*?\S)\1/g, '$2')
+      .replace(/(`{3,})(.*?)\1/gm, '$2')
+      .replace(/^-{3,}\s*$/g, '')
+      .replace(/`(.+?)`/g, '$1')
+      .replace(/\n{2,}/g, '\n\n');
+    return output;
+  }
 }
