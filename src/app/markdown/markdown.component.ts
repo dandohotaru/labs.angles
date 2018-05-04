235eab23f2d45f4d48946e2b9e27e1f3af65f76a
Dan Dohotaru <dan.dohotaru@gmail.com>
2018-05-04 11:56:05 +0200

    integrate simple mde deal with both scripts and styles include font awesome to avoid being served from cdn.

--- src/app/markdown/markdown.component.ts
+++ src/app/markdown/markdown.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ViewChild, ElementRef, InjectionToken, Inject } from '@angular/core';
+import * as SimpleMDE from 'simplemde';
 
 @Component({
   selector: 'app-markdown',
   templateUrl: './markdown.component.html',
+  styleUrls:['./markdown.component.css']
 })
-export class MarkdownComponent implements OnInit {
+export class MarkdownComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('simplemde') 
+  public textarea: ElementRef;
+
+  private simplemde: SimpleMDE;
+
+  constructor() {
+    
+   }
 
   public ngOnInit() {
+  }
+
+  public ngAfterViewInit(): void {
+    let config = {
+      element : this.textarea.nativeElement,
+      autoDownloadFontAwesome: false,
+      spellChecker:false,
+    };
+    this.simplemde = new SimpleMDE(config);
+
+  //   this.ngZone.runOutsideAngular(() => {
+  //     // code which configures and creates SimpleMDE
+  // });
+  }
+
+  public ngOnDestroy(): void {
+    this.simplemde = null
   }
 
 }
