632b7472eb61aa1ae69146432be9284572637346
Dan Dohotaru <dan.dohotaru@gmail.com>
2018-05-14 16:37:56 +0200

    move components to dedicated shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
+import { MarkdownModule } from './shared/components/markdown/markdown.module';
+import { FieldModule } from './shared/components/fields/field.module';
+import { TagsModule } from './shared/components/tags/tags.module';
+import { StarsModule } from './shared/components/stars/stars.module';
+import { RatesModule } from './shared/components/stars/rates.module';
 import { ValidationModule } from './shared/validators/validation.module';
 
 import { AppComponent } from './app.component';
-import { MarkdownModule } from './markdown/markdown.module';
-import { FieldModule } from './fields/field.module';
-import { TagsModule } from './tags/tags.module';
-import { StarsModule } from './stars/stars.module';
-import { RatesModule } from './stars/rates.module';
 
 @NgModule({
   imports: [
